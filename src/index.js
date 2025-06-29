b90b89f2e6b0cf331b60432d729e954fe3ebe2ec
alle0017 <allegretti0017@gmail.com>
2025-06-29 16:54:07 +0200

    default library defined

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import { useRendering } from "./rendering/Rendering.js";
-import Image from "./rendering/shader/lib/buffer/Image.js";
+import { useGame,Game } from "./game/Game.js";
+import Scene from "./game/Scene.js";
+import SceneManager from "./game/SceneManager.js";
+import { usePhysicsSystem } from "./lib/Physics.js";
+import { useSprite } from "./lib/Sprite.js";
+import { usePosition, usePhysicsPosition } from "./lib/Position.js";
 
-Image
-.preload("./.assets/t.jpg")
-.then(() => {
-      const [ctx,engine] = useRendering();
-      const img1 = ctx.image();
-      const img2 = ctx.image();
+export {
+      useGame,
+      Game,
+      Scene,
+      SceneManager,
+      usePhysicsPosition,
+      usePhysicsSystem,
+      useSprite,
+      usePosition,
+}
 
-      img1.image = "./.assets/t.jpg";
-      img2.image = "./.assets/t.jpg";
-      img1.scaleX = 0.5;
-      img1.scaleY = 0.5;
-      img2.scaleX = 0.3;
-      img2.scaleY = 0.3;
-      img2.x = -0.5;
-      img1.x = 0.5;
-      engine.add(img2)
-      engine.add(img1)
-});
+export const useScene = () => new Scene();
