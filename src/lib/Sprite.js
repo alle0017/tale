1c749b8f2794fb1f873180d959577a422145a1f6
alle0017 <allegretti0017@gmail.com>
2025-07-01 14:00:40 +0200

    added unsubscription method to unbind camera to position component

--- src/lib/Sprite.js
+++ src/lib/Sprite.js
@@ -9,6 +9,7 @@ import { useGame } from "../game/Game.js"
 export const useSprite = asset => {
       const game = useGame();
       const sprite = game.ctx.image();
+      let ticket;
 
       sprite.image = asset;
       game.engine.add(sprite);
@@ -19,15 +20,32 @@ export const useSprite = asset => {
              * bind the position component
              * to the sprite, so whenever the
              * position component changes the sprite
-             * will follow it
+             * will follow it. Every position that 
+             * was previously bind will be unbind
              * @param {Position} position 
              */
             bind(position) {
-                  position.onMove(pos => {
+                  if (ticket) {
+                        ticket();
+                  }
+
+                  ticket = position.onMove(pos => {
                         sprite.x = pos.x;
                         sprite.y = pos.y;
                   });
             },     
+            /**
+             * detach previously bound 
+             * position.
+             * @throws {Error} if no position was bound
+             */
+            unbind() {
+                  if (!ticket) {
+                        throw new Error('unbinding failed: position was not bound');
+                  }
+                  ticket();
+                  ticket = undefined;
+            },
             /**
              * stop rendering the sprite onto
              * the screen
--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,7 +1,7 @@
 export type Position = {
       x: number,
       y: number;
-      onMove(callback: (pos: Position) => void): void;
+      onMove(callback: (pos: Position) => void): () => void;
 }
 
 export type PhysicsPosition = {
@@ -11,5 +11,29 @@ export type PhysicsPosition = {
       vy: number,
       ax: number,
       ay: number,
-      onMove(callback: (pos: Position) => void): void;
+      onMove(callback: (pos: Position) => void): () => void;
+}
+
+/**
+ * x should be considered the leftmost 
+ * point onto the rectangle, while y should be considered
+ * the topmost point onto the rectangle.
+ * ### example
+ * 
+ * ---
+ * \
+ * ```plaintext
+ * a +---+ b
+ *   |   |
+ * c +---+ d
+ * ```
+ * 
+ * `c` is the (x,y) point used to check conditions
+ */
+export type RigidBody = {
+      x: number,
+      y: number,
+      width: number,
+      height: number,
+      onCollision(body: RigidBody): void;
 }
