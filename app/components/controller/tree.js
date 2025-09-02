8c5d23d4142045ccd7623997481aa3604fc2f77c
alle0017 <allegretti0017@gmail.com>
2025-09-02 21:44:04 +0200

    tree component

--- app/components/controller/tree.js
+++ app/components/controller/tree.js
@@ -1,14 +1,14 @@
 import { $effect, html } from "@alle0017!/photonjs";
-import { visible, tree,clickHandler, push } from "../../stores/tree";
+import { visible, tree,onClick, push } from "../../stores/tree";
 import Tree from "../ui/tree";
 export function TreeController() {
       return html`
             <div style="position: absolute; left: 0px; top: 0px; border-right: var(--border0);">
                   ${
                         $effect(() => visible.value?
-                              Tree({ content: tree.value, onClick: clickHandler.value }):
+                              Tree({ content: tree.value, onClick: onClick.value }):
                               [],
-                        visible, tree, clickHandler)
+                        visible, tree, onClick)
                   }
             </div>
       `
