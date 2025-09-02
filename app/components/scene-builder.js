79c7d39330242a9068225ff6db32ff9cbf637c44
alle0017 <allegretti0017@gmail.com>
2025-09-02 21:45:49 +0200

    scene builder

--- app/components/scene-builder.js
+++ app/components/scene-builder.js
@@ -1,23 +1,28 @@
-import { $watcher } from "@alle0017!/photonjs";
+import { $watcher, html, $signal } from "@alle0017!/photonjs";
 import * as BottomBar from "../stores/bottom-bar";
 import * as Drawer from "../stores/drawer";
 import * as Tree from "../stores/tree";
-import { _state, createScene, scenes, derive, addEntityToScene } from "../stores/project/project";
-export default function SceneBuilder() {
-      $watcher(() => {
-            BottomBar.replaceAll(Object.keys(scenes.value).map(scene => ({
-                        name: scene,
-                        image: './icons/scene.svg'
-                  })
-            ))
-      }, scenes);
+import { createScene, scenes, derive, addEntityToScene } from "../stores/project/project";
+import { ComponentsAccordion } from "./ui/component-accordion";
 
+/**
+ * 
+ * @param {() => void} callback 
+ * @param  {import("@alle0017!/photonjs").Reactive<unknown>} subs 
+ */
+const $bind = (callback, subs) => {
+      $watcher(callback, subs);
+      callback();
+}
+const setUpEvents = () => {
+      const body = $signal(html``);
+
+      Drawer.append(html`<div>${body}</div>`);
       BottomBar.onClick.value = scene => {
-            console.log(scene)
             const {accessor} = derive('scenes', scene);
             const entities = accessor(scene => scene.entities);
 
-            $watcher(() => {
+            $bind(() => {
                   Tree.replaceAll(entities.value.map(entity => ({
                         name: entity.name,
                         tooltip: `${entity.name}  [${entity.type}]`,
@@ -26,18 +31,37 @@ export default function SceneBuilder() {
                   })));
             }, entities);
       };
+      Tree.onClick.value = target => {
+            body.value = ComponentsAccordion({ entity: /**@type {EntityInstance}*/(target.data) })    
+      };
+}
+export default function SceneBuilder() {
+      setUpEvents();
+      $bind(() => {
+            BottomBar.replaceAll(Object.keys(scenes.value).map(scene => ({
+                        name: scene,
+                        image: './icons/scene.svg'
+                  })
+            ))
+      }, scenes);
 
-      BottomBar.replaceAll(Object.keys(scenes.value).map(scene => ({
-                  name: scene,
-                  image: './icons/scene.svg'
-            })
-      ));
       setTimeout(() => {
             createScene('Default');
             addEntityToScene('Default', {
-                  components: [],
-                  type: 'monster',
+                  components: [{
+                        name: 'Position',
+                        attributes: [{
+                              name: 'x',
+                              type: 'number',
+                              value: '0.0'
+                        }, {
+                              name: 'y',
+                              type: 'number',
+                              value: '0.0'
+                        }]
+                  }],
+                  type: 'None',
                   name: 'Anonymous'
             })
-      }, 1000)
+      }, 100)
 }
