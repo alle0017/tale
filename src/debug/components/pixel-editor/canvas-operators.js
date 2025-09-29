d5314e328956c0ace3de1c4adcb349b9c6167fc4
alle0017 <allegretti0017@gmail.com>
2025-09-29 21:15:10 +0200

    circle drawer operator

--- src/debug/components/pixel-editor/canvas-operators.js
+++ src/debug/components/pixel-editor/canvas-operators.js
@@ -40,3 +40,38 @@ export function drawLayer(ctx, layer, width, height) {
             }
       }
 }     
+
+/**
+ * set a pixel in safe way
+ * @param {string[][]} matrix 
+ * @param {number} x 
+ * @param {number} y 
+ * @param {string} value 
+ */
+function setPixel(matrix, x, y, value) {
+      if (y >= 0 && y < matrix.length && x >= 0 && x < matrix[0].length) {
+            matrix[y][x] = value;
+      }
+}
+
+/**
+ * 
+ * @param {string[][]} matrix 
+ * @param {number} cx 
+ * @param {number} cy 
+ * @param {number} r 
+ * @param {string} value 
+ * @returns 
+ */
+export function drawCircle(matrix, cx, cy, r, value) {
+      if (r < 0) {
+            return;
+      }
+      const steps = Math.max(8, Math.ceil(2 * Math.PI * r * 1.2)); // ~circumference sampling
+      for (let i = 0; i < steps; i++) {
+            const a = (i / steps) * 2 * Math.PI;
+            const x = Math.round(cx + r * Math.cos(a));
+            const y = Math.round(cy + r * Math.sin(a));
+            setPixel(matrix, x, y, value);
+      }
+}
