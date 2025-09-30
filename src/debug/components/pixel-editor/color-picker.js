b5bc8e7d0f11572827c89e866c81dac3776bdeca
alle0017 <allegretti0017@gmail.com>
2025-09-30 11:24:10 +0200

    color picker resize to fit better

--- src/debug/components/pixel-editor/color-picker.js
+++ src/debug/components/pixel-editor/color-picker.js
@@ -25,7 +25,7 @@ export default function ColorPicker({ onSelect }) {
       }
 
       return html`
-            <div style="height: 70%; width: 200px; color: var(--color); background-color: var(--bg); border-radius: 7px; padding: 10px; border: 1px solid var(--bg2); display: flex; flex-direction: column; gap: 20px; overflow-y: scroll; overflow-x: hidden;">
+            <div style="height: 50%; width: 200px; color: var(--color); background-color: var(--bg); border-radius: 7px; padding: 10px; border: 1px solid var(--bg2); display: flex; flex-direction: column; gap: 20px; overflow-y: scroll; overflow-x: hidden;">
                   <input type="color" @change=${e => append(e.target.value)} alpha=${true}/>
                   <div style="display: grid; grid-template-columns: auto auto; row-gap: 20px;">
                         ${stack.map(color => html`
