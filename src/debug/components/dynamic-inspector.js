96c6567a646e771f1a04d7bd6140a5f8e691de70
alle0017 <allegretti0017@gmail.com>
2025-10-02 08:28:23 +0200

    dynamic inspector component

---
+++ src/debug/components/dynamic-inspector.js
@@ -0,0 +1,28 @@
+import { html, } from "../../../node_modules/@alle0017!/photonjs/index.js"
+import css from "./css.module.js";
+import { view } from "./main.js";
+/**@import {Signal, VNode} from  "../../../node_modules/@alle0017!/photonjs/index.js"*/
+
+export default function DynamicInspector() {
+      return html`
+            <style>${css}</style>
+            <div style="height: 20px; position: fixed; top: 10%; width: 300px; left: 10px; color: var(--color); background-color: var(--bg); border-radius: 7px; padding: 10px; border: 1px solid var(--bg2); display: flex; flex-direction: column; gap: 20px; overflow-y: scroll; overflow-x: hidden;">
+                  <span 
+                  class="hv"
+                  @click=${() => {
+                        const popup = window.open(
+                              "https://www.pixilart.com/draw", // URL
+                              "popupWindow",         // Window name (reusing name reopens same popup)
+                              "width=600,height=400,left=200,top=100,resizable=yes,scrollbars=yes"
+                        );
+                        popup.close();
+                  }}>
+                        Pixel editor
+                  </span>
+            </div>
+            <div style="height: 60%; position: fixed; top: 20%; width: 300px; left: 10px; color: var(--color); background-color: var(--bg); border-radius: 7px; padding: 10px; border: 1px solid var(--bg2); display: flex; flex-direction: column; gap: 20px; overflow-y: scroll; overflow-x: hidden;">
+                  <CollisionWatcher/>
+                  ${view}
+            </div>
+      `     
+}
