a611e8b608f75d332b05c5550c9e433d27df9fa6
alle0017 <allegretti0017@gmail.com>
2025-10-12 21:07:51 +0200

    pipe implementation to have function driven rendering

---
+++ src/rendering/pipe/pipe.js
@@ -0,0 +1,46 @@
+/**
+ * @typedef {{ 
+ * x: number, 
+ * y: number, 
+ * z: number, 
+ * color: import("../rendering/grid").HexColor,
+ * primitive?: string,
+ * }} Pixel
+ */
+
+import OrderedList from "../../types/OrderedList.js";
+
+export class Pipe {
+      /**
+       * @type {OrderedList<(pixel: Pixel) => Pixel | undefined>}
+       */
+      #pipe = new OrderedList();     
+
+      /**
+       * 
+       * @param {Pixel} pixel 
+       */
+      apply(pixel) {
+            for (const filter of this.#pipe) {
+                  pixel = filter(pixel);
+
+                  if (!pixel) {
+                        return;
+                  }
+            }
+      }
+      /**
+       * 
+       * @param {(pixel: Pixel) => Pixel | undefined} filter 
+       */
+      use(filter) {
+            this.#pipe.push(filter);
+      }
+      /**
+       * 
+       * @param {(pixel: Pixel) => Pixel | undefined} filter 
+       */
+      remove(filter) {
+            this.#pipe.delete(filter);
+      }
+}
---
+++ src/types/OrderedList.js
@@ -0,0 +1,112 @@
+/**@import {ListNode} from "./List" */
+
+/**
+ * @template T
+ */
+export default class OrderedList {
+      /**
+       * @type {ListNode<T>}
+       */
+      #head;
+
+      /**
+       * @template T
+       * @param {T} value 
+       * @returns {ListNode<T>}
+       */
+      #node(value) {
+            return {
+                  next: null,
+                  value,
+            }
+      }
+      get length() {
+            return [...this].length;
+      }
+
+
+      /**
+       * @param {ListNode<T>} node 
+       */
+      append(node) {
+            if (!this.#head) {
+                  this.#head = node;
+                  return;
+            }
+            let curr = this.#head;
+
+            while (curr.next) {
+                  curr = curr.next;
+            }
+
+            curr.next = node;
+      }
+
+      /**
+       * @param {(value: T) => void} callback
+       */
+      forEach(callback) {
+            let head = this.#head;
+
+            while (head) {
+                  callback(head.value);
+                  head = head.next;
+            }
+      }
+      /**
+       * @param {T} value 
+       */
+      has(value) {
+            let head = this.#head;
+
+            while (head) {
+                  if (head.value === value) {
+                        return true;
+                  }
+                  head = head.next;
+            }
+            return false;
+      }
+      /**
+       * @param {T} value
+       */
+      push(value) {
+            const n = this.#node(value);
+
+            this.append(n);
+
+            return n;
+      }
+      /**
+       * @param {T} value 
+       */
+      delete(value) {
+            let prev = this.#head;
+            let head = this.#head;
+            
+            if (head && value == head.value) {
+                  this.#head = head.next;
+                  return true;
+            }
+            while (head) {
+                  if (head.value === value) {
+                        prev.next = head.next.next;
+                        return true;
+                  }
+                  prev = head;
+                  head = head.next;
+            }
+            return false;
+      }
+      clear() {
+            this.#head = null;
+      }
+      * [Symbol.iterator]() {
+            let head = this.#head;
+
+            while (head) {
+                  yield head.value;
+                  head = head.next;
+            }
+      }
+}
