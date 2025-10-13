fcfbe6bcc77c5eae55a3074390e57d29e027d5f9
alle0017 <allegretti0017@gmail.com>
2025-10-13 20:26:43 +0200

    Drawable object

--- src/lib/Drawable.js
+++ src/lib/Drawable.js
@@ -1,11 +1,12 @@
 import GPUEntity2D from "../rendering/shader/entity/GPUEntity2D.js";
 import { createAbstractComponent, createComponent } from "../ecs/Component.js";
 import { useGame } from "../game/Game.js";
+/**@import { Shader } from "../rendering/shaders/shader.js";*/
 /**@import {Query, Component} from "../ecs/Component.js";*/
 /**
  * prototype to assign to any component
  * that should be queried by the renderer
- * @type {Component<GPUEntity2D,unknown[]>}
+ * @type {Component<Shader,unknown[]>}
  */
 export const Drawable = createAbstractComponent();
 /**
@@ -17,11 +18,5 @@ export const Drawable = createAbstractComponent();
  */
 export const createDrawable = (factory, ...prototypes) => {
       const component = createComponent(factory, Drawable, ...prototypes);
-      /*component.events.on('attached', () => {
-            useGame().worlds.current.events.trigger('change');
-      });
-      component.events.on('removed', () => {
-            useGame().worlds.current.events.trigger('change');
-      });*/
       return component;
 };
