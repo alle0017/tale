15bfa4a4ee4ae9d45ac406c3fdad71e64ed0dc07
alle0017 <allegretti0017@gmail.com>
2025-10-13 20:26:17 +0200

    cleaner game  object. decoupled graphics from game object

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,8 +1,4 @@
 import { WorldManager } from "../ecs/WorldManager.js";
-import Context from "../rendering/Context.js";
-import Image from "../rendering/shader/lib/buffer/Image.js";
-/**@import GPUEntity2D from "../rendering/shader/entity/GPUEntity2D.js" */
-/**@import GPUContext from "../rendering/index.js" */
 
 
 export class Game {
@@ -11,21 +7,7 @@ export class Game {
       static get() {
             return Game.#game;
       }
-      /**
-       * @type {GPUContext}
-       */
-      #ctx;
       debug = false;
-
-      /**
-       * the camera used inside the scene.
-       * every default entity is bound to this
-       * camera.
-       */
-      get camera() {
-            return this.#ctx.camera;
-      }
-
       /**
        * Context used to draw entities onto the canvas.
        * To render an entity, it must be created with the
@@ -46,9 +28,6 @@ export class Game {
        * f();
        * ```
        */
-      get ctx() {
-            return this.#ctx;
-      }
       /**
        * scene manager useful to transit across Worlds 
        * and preserve their state
@@ -61,23 +40,6 @@ export class Game {
        * @private
        */
       constructor() {
-            this.#ctx = new Context();
-      }
-
-      /**
-       * method used to preload all images 
-       * that will be used inside the game
-       * @param  {Record<string,string>} imgs 
-       */
-      async preload(imgs) {
-
-            for (const[k,v] of Object.entries(imgs)) {
-                  try {
-                        await Image.preload(v, k);
-                  } catch (e) {
-                        console.error(e);
-                  }
-            }
       }
 }
 
