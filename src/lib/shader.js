aaf076350285203194fc7c983ec43bdabc774999
alle0017 <allegretti0017@gmail.com>
2025-10-29 22:11:25 +0100

    shader component

---
+++ src/lib/shader.js
@@ -0,0 +1,34 @@
+import { createDrawable, } from "./Drawable.js";
+/**@import Screen from "../rendering/screen/screen.js";*/
+/**@import {Coordinates} from "./index.d.ts" */
+
+/**
+ * @type {import("../ecs/Component.js").Component<{
+ *    draw(screen: Screen): void, 
+ *    state: {}
+ * }, [(screen: Screen, state: {}) => void, state: {}]>}
+ */
+export const Shader = createDrawable(
+      /**
+       * @template {{}} T
+       * @param {T} state 
+       * @param {(screen: Screen, state: T) => void} shader 
+       * @returns {{ draw(screen: Screen): void, state: T }}
+       */
+      (shader, state) => {
+
+            return {
+                  /**
+                   * @type {T}
+                   */
+                  state,
+                  /**
+                   * 
+                   * @param {Screen} screen 
+                   */
+                  draw(screen) {
+                        shader(screen, this.state);
+                  }
+            };
+      }
+);
